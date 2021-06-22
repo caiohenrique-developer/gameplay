dfb4206463ad8a8e19ed67d1e5faae2a54ce6da0
caiohenrique-developer <caiorussellrox@msn.com>
2021-06-22 16:25:12 -0300

    :sparkles: [ FEAT ]: Add fonts and linear gradient background page

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Text, View, Image, StatusBar } from "react-native";
+import { Text, View, Image } from "react-native";
 
 import { ButtonIcon } from "../../components/ButtonIcon";
 
@@ -8,16 +8,13 @@ import { styles } from "./styles";
 import IllustrationImg from "../../assets/illustration.png";
 
 export function SignIn() {
-
   return (
     <View style={styles.container}>
-      <StatusBar 
-        barStyle="light-content"
-        backgroundColor="transparent"
-        translucent
+      <Image
+        source={IllustrationImg}
+        style={styles.image}
+        resizeMode="stretch"
       />
-      
-      <Image source={IllustrationImg} style={styles.image} resizeMode="stretch" />
 
       <View style={styles.content}>
         <Text style={styles.title}>
--- src/screens/SignIn/styles.ts
+++ src/screens/SignIn/styles.ts
@@ -6,26 +6,29 @@ export const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: theme.colors.background,
   },
   image: {
-    width: '100%',
+    width: "100%",
     height: 360,
   },
   content: {
-    marginTop: -88,
-    paddingHorizontal: 50
+    marginTop: -60,
+    paddingHorizontal: 50,
   },
   title: {
     color: theme.colors.heading,
-    textAlign: 'center',
+    textAlign: "center",
     fontSize: 40,
-    marginBottom: 16
+    marginBottom: 16,
+    fontFamily: theme.fonts.title700,
+    lineHeight: 40,
   },
   subTitle: {
     color: theme.colors.heading,
-fontSize: 15,
-textAlign: 'center',
-marginBottom: 64
+    fontSize: 15,
+    textAlign: "center",
+    marginBottom: 64,
+    fontFamily: theme.fonts.title500,
+    lineHeight: 25,
   },
 });
