d524485e281c70c99008484e7e08e8746bfac994
caiohenrique-developer <caiorussellrox@msn.com>
2021-06-23 21:44:21 -0300

    :sparkles: [ FEAT ]: Create appointment details banner

--- src/screens/AppointmentDetails/index.tsx
+++ src/screens/AppointmentDetails/index.tsx
@@ -1,12 +1,31 @@
 import React from "react";
+import { ImageBackground } from "react-native";
+import { BorderlessButton } from "react-native-gesture-handler";
+import { Fontisto } from "@expo/vector-icons";
 
 import { Background } from "../../components/Background";
 import { Header } from "../../components/Header";
+import { theme } from "../../global/styles/theme";
+
+import BannerImg from "../../assets/banner.png";
+import { styles } from "./styles";
 
 export function AppointmentDetails() {
   return (
     <Background>
-      <Header title="Detalhes" />
+      <Header
+        title="Detalhes"
+        action={
+          <BorderlessButton>
+            <Fontisto name="share" size={20} color={theme.colors.primary} />
+          </BorderlessButton>
+        }
+      />
+
+      <ImageBackground
+        source={BannerImg}
+        style={styles.banner}
+      ></ImageBackground>
     </Background>
   );
 }
--- src/screens/AppointmentDetails/styles.ts
+++ src/screens/AppointmentDetails/styles.ts
@@ -1,5 +1,11 @@
 import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
-  container: {},
+  container: {
+    flex: 1,
+  },
+  banner: {
+    width: "100%",
+    height: 234,
+  },
 });
