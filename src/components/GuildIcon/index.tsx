1f50674cbff2a73a168c73cdcd93d63205f8aeb1
caiohenrique-developer <caiorussellrox@msn.com>
2021-07-01 13:57:40 -0300

    :sparkles: [ FEAT ]: Create Load component, get server channels from discord

--- src/components/GuildIcon/index.tsx
+++ src/components/GuildIcon/index.tsx
@@ -1,11 +1,26 @@
 import React from "react";
-import { Image } from "react-native";
+import { Image, View } from "react-native";
 
 import { styles } from "./styles";
+import DiscordSvg from "../../assets/discord.svg";
 
-export function GuildIcon() {
-  const uri =
-    "http://pm1.narvii.com/6427/1aa160baa0c1d44c5b3350999ef2b66abba6be41_00.jpg";
+const { CDN_IMAGE } = process.env;
 
-  return <Image style={styles.image} source={{ uri }} resizeMode="cover" />;
+type Props = {
+  guildId: string;
+  iconId: string | null;
+};
+
+export function GuildIcon({ guildId, iconId }: Props) {
+  const uri = `${CDN_IMAGE}/icons/${guildId}/${iconId}.png`;
+
+  return (
+    <View style={styles.container}>
+      {iconId ? (
+        <Image style={styles.image} source={{ uri }} resizeMode="cover" />
+      ) : (
+        <DiscordSvg width={40} height={40} />
+      )}
+    </View>
+  );
 }
